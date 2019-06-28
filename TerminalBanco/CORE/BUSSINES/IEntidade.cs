713ca8b2bf1713d6214f61689950db194fbc2df3
vanessalucion <40900218+vanessalucion@users.noreply.github.com>
2019-06-28 20:33:39 -0300

    28_06

---
+++ TerminalBanco/CORE/BUSSINES/IEntidade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CORE.BUSSINES
+{
+    interface IEntidade<T>
+    {
+        void Novo(T registro);
+        void Salvar(T registro);
+        void Excluir(T registro);
+        List<T> Lista();
+    }
+}
---
+++ TerminalBanco/CORE/BUSSINES/OnConta.cs
@@ -0,0 +1,32 @@
+﻿using CORE.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CORE.BUSSINES
+{
+    public class OnConta : IEntidade<Conta>
+    {
+        public void Excluir(Conta registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Conta> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Novo(Conta registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Salvar(Conta registro)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
